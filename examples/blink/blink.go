8978ceb81032e32eba53b5e2a645103a18e5d3e0
iwakunibob <totorobob@protonmail.com>
2024-01-22 00:37:41 -0600

    led

--- examples/blink/blink.go
+++ examples/blink/blink.go
@@ -13,15 +13,17 @@ import (
 func main() {
 	// set GPIO25 to output mode
 
-	pin22, err := gpio.OpenPin(rpi.GPIO22, gpio.ModeOutput)
-	if err != nil {
-		fmt.Printf("Error opening pin! %s\n", err)
-		return
-	}
-	pin23, err := gpio.OpenPin(rpi.GPIO23, gpio.ModeOutput)
-	if err != nil {
-		fmt.Printf("Error opening pin! %s\n", err)
-		return
+	outputs := []uint{rpi.GPIO22, rpi.GPIO23, rpi.GPIO24, rpi.GPIO25} //,  rpi.GPIO25}
+	colorLED := []string{"Red", "Yellow", "Green", "Blue"}
+	var outPins [4]gpio.Pin
+	fmt.Println(outputs)
+	for i, outp := range outputs {
+		pin, err := gpio.OpenPin(int(outp), gpio.ModeOutput)
+		outPins[i] = pin
+		if err != nil {
+			fmt.Printf("Error opening %d pin = %v, Error: %s\n", outp, pin, err)
+			return
+		}
 	}
 
 	// turn the led off on exit
@@ -29,22 +31,22 @@ func main() {
 	signal.Notify(c, os.Interrupt)
 	go func() {
 		for _ = range c {
-			fmt.Printf("\nClearing and unexporting the pin.\n")
-			pin22.Clear()
-			pin23.Clear()
-			pin22.Close()
-			pin23.Close()
+			fmt.Printf("\nClearing and unexporting the pins.\n")
+			for _, pin := range outPins {
+				pin.Clear()
+				pin.Close()
+			}
 			os.Exit(0)
 		}
 	}()
 
-	for j := 1; j <= 15; j++ {
-		pin22.Set()
-		pin23.Clear()
-		time.Sleep(200 * time.Millisecond)
-		pin23.Set()
-		pin22.Clear()
-		time.Sleep(300 * time.Millisecond)
-		fmt.Printf("Blink %v\n", j)
+	for j := 1; j <= 8; j++ {
+		for k, pin := range outPins {
+			pin.Set()
+			time.Sleep(700 * time.Millisecond)
+			pin.Clear()
+			time.Sleep(300 * time.Millisecond)
+			fmt.Printf("Loop %d Output %s\n", j, colorLED[k])
+		}
 	}
 }
